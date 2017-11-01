6f05e33321e4671264cd3aaa854facbc7c537347
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-01 00:31:45 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ result = []
+ var = false
+ while i < collection.length
+   var = yield collection[i]
+   if var == true
+     result << collection[i]
+   end
+   i += 1
+ end
+ result
 end
